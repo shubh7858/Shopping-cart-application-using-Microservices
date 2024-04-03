58aeab3e8cfc0edd321438196e0828d8c66c8a7f
shubh7858 <shubham7858.sp@gmail.com>
2024-04-03 15:08:28 +0530

    OKta Auth2 Authentication

--- CloudGateway/src/main/java/com/shubhampatil/CloudGateway/Model/AuthenticationResponse.java
+++ CloudGateway/src/main/java/com/shubhampatil/CloudGateway/Model/AuthenticationResponse.java
@@ -1,7 +1,15 @@
 package com.shubhampatil.CloudGateway.Model;
 
-import java.util.Collection;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Collection;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AuthenticationResponse {
     private String userId;
     private String accessToken;
